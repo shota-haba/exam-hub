e9c96ba62a333325f067979fda6ec4ac723a16be
shota haba <shota.haba.bp@gmail.com>
2025-06-15 22:19:20 +0900

    Updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Header from '@/components/shared/Header'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import Link from 'next/link'
