8b17991c460958713b13e59bddbfa19cac1fb171
shota haba <shota.haba.bp@gmail.com>
2025-06-15 22:28:14 +0900

    Fix Header component reference error

--- next.config.js
+++ next.config.js
@@ -4,14 +4,6 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-  // Add webpack configuration to handle cache issues
-  webpack: (config, { dev, isServer }) => {
-    // Use in-memory caching in development to prevent file system errors
-    if (dev) {
-      config.cache = { type: 'memory' };
-    }
-    return config;
-  },
 };
 
 module.exports = nextConfig;
