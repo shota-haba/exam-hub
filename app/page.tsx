603986a3ae1a46c4491ccc4d49bac9c09548ddcd
shota haba <shota.haba.bp@gmail.com>
2025-06-15 22:19:54 +0900

    Updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,6 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col bg-background">
-      <Header />
-      
       <main className="flex-1">
         <section className="py-20 px-4 border-b">
           <div className="container max-w-4xl mx-auto text-center">
